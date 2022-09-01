01c99709c39ec8f3a43bbdb5c39c345d7f73e4cc
Juan Ramirez <juanpram2000@gmail.com>
2022-09-01 14:15:56 -0700

    fixed some path issues

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,"./build")));
+app.use(express.static(path.join(__dirname,"../build")));
 app.use(express.static('dist'));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://miki-photobook.wl.r.appspot.co")
@@ -41,7 +41,7 @@ app.get('/api/findPlace', async (req: Request, res: Response) => {
     }
 });
 
-app.get('/*', (req, res) => {
+app.get('../*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
